f73f9f0c2c7e3d387aba56aacf312aba7a7b1fa2
Drilon Halili <drilon.halili@gmail.com>
2021-03-11 23:51:13 +0100

    SignUp action and connect

---
+++ src/store/actions/authActions.js
@@ -0,0 +1,16 @@
+// Sign up action creator
+export const signUp = (data) => async (
+  dispatch,
+  getState,
+  { getFirebase, getFirestore }
+) => {
+  const firebase = getFirebase();
+  const firestore = getFirestore();
+  try {
+    const res = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(data.email, data.password);
+
+    console.log(res);
+  } catch (err) {}
+};
